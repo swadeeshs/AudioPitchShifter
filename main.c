c2b2759573d9ec0a15ad052477266bc607ab2d44
swadeesh srivatsa m r <38063415+swadeeshs@users.noreply.github.com>
2019-05-24 20:25:21 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,79 +1,70 @@
 #include "dsk6713_aic23.h"
 #define BUFF_LEN 1024
+#define N 7
 
-#define DIV 4
 
 Uint32 fs = DSK6713_AIC23_FREQ_8KHZ;
 #define DSK6713_AIC23_INPUT_MIC 0x0015
 #define DSK6713_AIC23_INPUT_LINE 0x0011
 
-int up = 0;
-int in_loop_index = 0;
-int skip_flag = 0;
-int buff[BUFF_LEN];
+
 Uint16 inputsource = DSK6713_AIC23_INPUT_LINE;
 
-int filter_in[7];
-//double filter_coeff[11] = { 0.0003,   -0.0001,    0.0002,   -0.0002,    0.0002,    0.9998,    0.0002,   -0.0002,    0.0002,   -0.0001,    0.0003};
-double filter_coeff[7] = {0.0003,   -0.0001,    0.0001,    0.9999,    0.0001,   -0.0001, 0.0003};
+int ring_buffer[BUFF_LEN] = 0;
+float index;
+float pitch_up = 0.85;
+float pitch_down = 1.25;
+float no_change = 1;
+float delta;
+int write_addr = 0;
+int read_addr = 0;
+
+
+int filter_in[N];
+
+double filter_coeff[N] = {0.0003,   -0.0001,    0.0001,    0.9999,    0.0001,   -0.0001, 0.0003};
+
+
 void output(int n) {
 	int i, j, sum = 0;
 
 	filter_in[0] = n;
 
-	for (i = 0; i < 7; i++) {
+	for (i = 0; i < N; i++) {
 	     sum += filter_coeff[i] * filter_in[i];
 	}
 
-	for (j = 6; j > 0; j--) {
+	for (j = N-1; j > 0; j--) {
 		filter_in[j] = filter_in[j - 1];
 	}
 
-
-
 	output_sample(sum);
-
 }
 
 
 void main() {
 
 	comm_poll();
-	int i;
 
 	DSK6713_DIP_init();
 
     while(1) {
 
     	if (DSK6713_DIP_get(0) == 1) {
-    		up = 1;
+    		delta = pitch_down;
     	} else if (DSK6713_DIP_get(1) == 1) {
-    		up = -1;
+    		delta = pitch_up;
     	} else {
-    		up = 0;
+    		delta = no_change;
     	}
 
-        buff[in_loop_index] = input_sample();
+    ring_buffer[write_addr] = input_sample();
+    write_addr++;
+    write_addr %= 1024;
 
-        if (up == 1) {
-            for (i = 0; i < DIV; i++) {
-//                output_sample(buff[in_loop_index]);
-            	output(buff[in_loop_index]);
-            	DSK6713_waitusec(125/DIV);
-            }
-        } else if (up == -1) {
-//        	output_sample(buff[in_loop_index]);
-        	output(buff[in_loop_index]);
-        	DSK6713_waitusec(125 * DIV);
-
-        } else {
-        	output(buff[in_loop_index]);
- //            output_sample(buff[in_loop_index]);
-        }
-
-        if (in_loop_index++ == (BUFF_LEN - 1)) {
-            in_loop_index = 0;
-        }
+    output(ring_buffer[read_addr]);
+    index = index + delta;
+    read_addr = (int)index % 1024;
 
     }
 }
